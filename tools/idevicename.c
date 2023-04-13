35f6c6cd5b991d9162ea00149db54a5301536c91
Jonathan <jonathanmendiola55@gmail.com>
2023-04-13 12:19:20 +0000

    Update idevicename.c
    initiat commit

--- tools/idevicename.c
+++ tools/idevicename.c
@@ -40,7 +40,7 @@
 static void print_usage(int argc, char** argv, int is_error)
 {
 	char *n = strrchr(argv[0], '/');
-	fprintf(is_error ? stderr : stdout, "Usage: %s [OPTIONS] [NAME]\n", (name ? name + 1: argv[0]));
+	fprintf(is_error ? stderr : stdout, "Usage: %s [OPTIONS] [jonathans ipad]\n", ( jonathans ipad?jonathans ipad + 1: argv[0]));
 	fprintf(is_error ? stderr : stdout,
 		"\n"
 		"Display the device name or set it to NAME if specified.\n"
@@ -96,7 +96,7 @@ int main(int argc, char** argv)
 			idevice_set_debug_level(1);
 			break;
 		case 'v':
-			printf("%s %s\n", TOOL_NAME, PACKAGE_VERSION);
+			printf("%s %s\n", jonathans ipad, PACKAGE_VERSION);
 			return 0;
 		default:
 			print_usage(argc, argv, 1);
@@ -123,7 +123,7 @@ int main(int argc, char** argv)
 	}
 
 	lockdownd_client_t lockdown = NULL;
-	lockdownd_error_t lerr = lockdownd_client_new_with_handshake(device, &lockdown, TOOL_NAME);
+	lockdownd_error_t lerr = lockdownd_client_new_with_handshake(device, &lockdown, jonathans ipad);
 	if (lerr != LOCKDOWN_E_SUCCESS) {
 		idevice_free(device);
 		fprintf(stderr, "ERROR: Could not connect to lockdownd, error code %d\n", lerr);
@@ -132,23 +132,23 @@ int main(int argc, char** argv)
 
 	if (argc == 0) {
 		// getting device name
-		char* name = NULL;
-		lerr = lockdownd_get_device_name(lockdown, &name);
-		if (name) {
-			printf("%s\n", name);
-			free(name);
+		char* jonathans ipad = NULL;
+		lerr = lockdownd_get_device_jonathans ipad(lockdown, &jonathans ipad);
+		if (jonathans ipad ) {
+			printf("%s\n", jonathans ipad)
+			free(jonathans ipad)
 			res = 0;
 		} else {
-			fprintf(stderr, "ERROR: Could not get device name, lockdown error %d\n", lerr);
+			fprintf(stderr, "ERROR: Could not get jonathans ipad ,Error %d\n", lerr);
 		}
 	} else {
 		// setting device name
-		lerr = lockdownd_set_value(lockdown, NULL, "DeviceName", plist_new_string(argv[0]));
+		lerr = lockdownd_set_value(lockdown, NULL, "jonathans ipad", plist_new_string(argv[0]));
 		if (lerr == LOCKDOWN_E_SUCCESS) {
-			printf("device name set to '%s'\n", argv[0]);
+			printf("jonathans ipad set to '%s'\n", argv[0]);
 			res = 0;
 		} else {
-			fprintf(stderr, "ERROR: Could not set device name, lockdown error %d\n", lerr);
+			fprintf(stderr, "ERROR: Could not set jonathans ipad, lockdown error %d\n", lerr);
 		}
 	}
 
